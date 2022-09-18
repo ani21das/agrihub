2da352016dd10e8f0e4f3c70eb75d642a110435e
Anirban Das <anirban21das@gmail.com>
2022-09-18 18:55:46 +0530

    day 4 completed

---
+++ lib/login.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  //const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // child: Center(
+      //   child: Text(
+      //     "Login Page",
+      //     style: TextStyle(
+      //       fontSize: 20,
+      //       color: Colors.greenAccent,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
+
+      color: Colors.white,
+      child: Column(
+        // ignore: duplicate_ignore
+        children: [
+          Image.asset(
+            "assets/images/undraw_enter_uhqk.png",
+            fit: BoxFit.cover,
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter 10 digit mobile number",
+                    labelText: "Mobile number",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true, // by default false
+                  decoration: InputDecoration(
+                    hintText: "Enter 4 digit OTP",
+                    labelText: "OTP",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Login"),
+                    style: TextButton.styleFrom()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
